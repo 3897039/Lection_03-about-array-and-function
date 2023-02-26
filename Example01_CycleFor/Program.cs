5e02c1acb26a98c8cd5a2edfab44d6838fa78cb1
Tatsiana Putryk <tushkanchik85@gmail.com>
2023-02-26 14:10:20 +0300

    about cycle FOR added

---
+++ Example01_CycleFor/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пример с циклом FOR(напечатать 10 раз букву Z)
+
+string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(10, "Z");
+Console.WriteLine(res);
+
